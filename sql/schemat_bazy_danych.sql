303f932b7ab23b00ef9c79b986f4d893542de00b
igasz <igaszaf@gmail.com>
2026-05-04 10:05:12 +0200

    schemat bazy

---
+++ sql/schemat_bazy_danych.sql
@@ -0,0 +1,57 @@
+create user spizarnia identified by 123;
+
+grant connect, resource to spizarnia
+
+alter user spizarnia quota unlimited on users;
+
+-- Użytkownicy
+CREATE TABLE Users (
+    User_ID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    First_Name VARCHAR2(50) NOT NULL,
+    Last_Name VARCHAR2(50) NOT NULL,
+    Role VARCHAR2(20) NOT NULL
+);
+
+-- Katalog Produktów
+CREATE TABLE Product_Catalog (
+    Product_ID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Product_Name VARCHAR2(100) NOT NULL,
+    Unit VARCHAR2(10) NOT NULL,
+    Min_Stock_Level NUMBER NOT NULL
+);
+
+-- spiżarnia
+CREATE TABLE Inventory (
+    Batch_ID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Product_ID NUMBER NOT NULL,
+    Quantity NUMBER NOT NULL,
+    Expiration_Date DATE NOT NULL,
+    Status VARCHAR2(20) NOT NULL,
+    FOREIGN KEY (Product_ID) REFERENCES Product_Catalog(Product_ID)
+);
+
+-- dania/menu
+CREATE TABLE Dishes (
+    Dish_ID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Dish_Name VARCHAR2(100) NOT NULL,
+    Category VARCHAR2(50) NOT NULL
+);
+
+-- przepisy
+CREATE TABLE Recipes (
+    Recipe_ID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Dish_ID NUMBER NOT NULL,
+    Product_ID NUMBER NOT NULL,
+    Required_Quantity NUMBER NOT NULL,
+    FOREIGN KEY (Dish_ID) REFERENCES Dishes(Dish_ID),
+    FOREIGN KEY (Product_ID) REFERENCES Product_Catalog(Product_ID)
+);
+
+-- lista zakupów
+CREATE TABLE Shopping_List (
+    Shopping_List_ID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Product_ID NUMBER NOT NULL,
+    Date_Added DATE DEFAULT SYSDATE,
+    Status VARCHAR2(20) DEFAULT 'TO_BUY',
+    FOREIGN KEY (Product_ID) REFERENCES Product_Catalog(Product_ID)
+);
